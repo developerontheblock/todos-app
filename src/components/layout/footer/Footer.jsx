504dbc323d433c76fe8092209eb4bc6893d9bd2b
Alex Draev <a.draev@codecoda.com>
2020-05-08 11:25:33 +0300

    added header and footer

---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const styles = {
+    backgroundColor: 'lightgray',
+    height: '20px',
+    width: '100%',
+    position: 'fixed',
+    bottom: 0
+};
+
+export function Footer(){
+    return(
+        <div className="footer" style={styles}>
+            Footer works
+        </div>
+
+    );
+}
