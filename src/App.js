525f948f35818fb9d6ef461f9afd71617fc5798e
Alex Draev <a.draev@codecoda.com>
2020-05-11 14:58:56 +0300

    NonAuthenticatedRoute added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Layout from "./components/layout/Layout";
 import { Switch, Route  } from 'react-router-dom';
 import { Login } from './components/auth/login/Login';
 import {AuthenticatedRoute} from './core/guards/AuthenticatedRoute';
+import {NonAuthenticatedRoute} from './core/guards/NonAuthenticatedRoute';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/login" component={Login}></Route>
+        <NonAuthenticatedRoute exact path="/login" component={Login} />
         <AuthenticatedRoute path="/" component={Layout} />
        </Switch>
     </div>
--- src/core/guards/NonAuthenticatedRoute.jsx
+++ src/core/guards/NonAuthenticatedRoute.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { getLoggedUser } from '../api/users.api'
 import { Redirect } from 'react-router-dom';
 
-export function AuthenticatedRoute(props){
+export function NonAuthenticatedRoute(props){
     const loggedUser = getLoggedUser();
 
-    if(loggedUser){
+    if(!loggedUser){
         return <props.component {...props} />
     }
 
-    return <Redirect to="/login" />
+    return <Redirect to="/" />
 }
