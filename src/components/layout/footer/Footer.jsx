f84a449e151b932db5ba416477cc911bf8f948b3
Alex Draev <a.draev@codecoda.com>
2020-05-28 16:12:02 +0300

    quick design changes[footer(make it sticky) & note card]

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
 
 const styles = {
-    position: 'absolute',
-    bottom: 0,
+    backgroundColor: 'lightgray',
+    height: '20px',
     width: '100%',
-    height: '60px',
-    lineHeight: '60px',
-    backgroundColor: '#f5f5f5'
+    position: 'fixed',
+    bottom: 0
 };
-
 
 export function Footer() {
     return (
         <div className="footer" style={styles}>
-            <div className="container">
-                <span className="text-muted">
-                    Faculty number: 1601681033
-                </span>
-            </div>
-
+            Footer works
         </div>
-
     );
 }
--- src/components/notes/note-card/NoteCard.jsx
+++ src/components/notes/note-card/NoteCard.jsx
@@ -16,23 +16,17 @@ export function NoteCard({ note, onDeleteClick }) {
 
     const loggedUser = getLoggedUser();
     return (
-
-
-//         <div class="card border-primary mb-3" style="max-width: 18rem;">
-//   <div class="card-header">Header</div>
-//   <div class="card-body text-primary">
-//     <h5 class="card-title">Primary card title</h5>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//   </div>
-
-
         <div className="card border-primary mb-2 ml-4 mt-2" style={noteCardStyle}>
+
+            <div className="card-body text-primary">
+                <h5 class="card-title">{note.title}</h5>
+            </div>
             <div className="card-header">
-                {note.title}
                 {(loggedUser.isAdmin || loggedUser.id === note.authorId) && <Link className="cursor-pointer btn btn-success mr-1 mt-1" to={`/notes/edit/${note.id}`}>Edit </Link>}
                 {(loggedUser.isAdmin || loggedUser.id === note.authorId) && <span className="cursor-pointer btn btn-danger mt-1" style={deleteBtnStyles} onClick={() => onDeleteClick(note.id)}>Delete</span>}
             </div>
-            <div className="card-body text-primary">
+
+            <div className="card-body ">
                 <p className="card-text">{note.content}</p>
             </div>
             <div className="card-footer bg-transparent border-secondary">
@@ -43,6 +37,7 @@ export function NoteCard({ note, onDeleteClick }) {
                     Created on: {note.date}
                 </div>
             </div>
+
         </div>
     );
 }
